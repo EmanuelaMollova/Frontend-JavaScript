486aea611f60318fba35a19681f59382a874d677
EmanuelaMollova <emanuela.mollova@gmail.com>
2014-06-03 15:13:46 +0300

    Refactor sum and concat

--- week1/01_sum/solution.js
+++ week1/01_sum/solution.js
@@ -1,10 +1,14 @@
 "use strict";
 
 var sum = function(a, b) {
-    if (typeof a === 'number' && typeof b === 'number') {
+    return sumForType(a, b, 'number');
+};
+
+var sumForType = function(a, b, type) {
+    if (typeof a === type && typeof b === type) {
         return a + b;
     } else {
-        throw new TypeError('Both arguments should be numbers.');
+        throw new TypeError('Both arguments should be ' + type + 's.');
     }
 };
 
--- week1/02_concat/solution.js
+++ week1/02_concat/solution.js
@@ -1,10 +1,14 @@
 "use strict";
 
 var concat = function(a, b) {
-    if (typeof a === 'string' && typeof b === 'string') {
+    return sumForType(a, b, 'string');
+};
+
+var sumForType = function(a, b, type) {
+    if (typeof a === type && typeof b === type) {
         return a + b;
     } else {
-        throw new TypeError('Both arguments should be strings.');
+        throw new TypeError('Both arguments should be ' + type + 's.');
     }
 };
 
